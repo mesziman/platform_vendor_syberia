LineageOS/android_vendor_lineage

prebuilt: Add a script to simplify A-only recovery system mount
* This can be helpful because of the fragmentation that will happen with the system mount in recovery after Q

Change-Id: I2d1e775efcf87e33319bc7790d1e54bca72116d3

config/common.mk
@@ -45,6 +45,10 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
 endif
 
+# system mount
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
+
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/backup.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/backup.xml

mesziman/platform_vendor_syberia

config/common.mk
@@ -53,6 +53,10 @@ PRODUCT_COPY_FILES += \
 $(foreach f,$(wildcard vendor/syberia/prebuilt/common/etc/init/*.rc),\
     $(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
 
+# system mount
+PRODUCT_COPY_FILES += \
+    vendor/syberia/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
+
 # Init files
 ifeq ($(AB_OTA_UPDATER),true)
 PRODUCT_COPY_FILES += \